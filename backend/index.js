dc2d5344ef6a5de04146a5b783d3612b17721911
Dell <prasuhirpara@gmail.com>
2024-07-18 19:25:04 +0530

    set up

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// View engine setup
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Define homepage route
-app.get('/', (req, res) => {
-    res.render('homepage');
-});
-
 // Connect to MongoDB and start server
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
