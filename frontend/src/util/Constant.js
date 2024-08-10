359309520388e7569de9dc8120de2204f81f0451
Dell <prasuhirpara@gmail.com>
2024-08-10 19:52:51 +0530

    change

--- frontend/src/util/Constant.js
+++ frontend/src/util/Constant.js
@@ -1,5 +1,5 @@
 const Constant = {
-    // BASE_URL: "https://stocktracker-hub.onrender.com"
-    BASE_URL: `http://localhost:10000`
+    BASE_URL: "https://stocktracker-hub.onrender.com"
+    // BASE_URL: `http://localhost:10000`
 }
 export default Constant;
